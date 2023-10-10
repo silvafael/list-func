7b14572f6c16e169d1453f7f7c9f62495f5553d6
Rafael Silva <117862576+silvafael@users.noreply.github.com>
2023-10-10 16:49:48 +0000

    update home

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
     IonicModule, 
-    ExploreContainerComponent,
     CommonModule,
     FormsModule,
   ],
